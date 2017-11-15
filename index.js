46b38c348c274263f774b1b5ae9b535c39036bf7
Adrienne Johnson <adriennetjohnson@gmail.com>
2017-11-15 02:51:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const driver = {name: "Bob", age: "42", yearsExperience: "3"};
 
+function updateDriverWithKeyAndValue(obj, key, value) {
+  const newDriver = Object.assign({}, driver, "hobby", "chess");
+  return newDriver;
+}
